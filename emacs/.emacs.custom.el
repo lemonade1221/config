c317476f097ee662bad584b461811a2b45ea1c6c
下一个到我 <1135179467@qq.com>
2026-03-03 11:49:33 +0800

    update file

--- emacs/.emacs.custom.el
+++ emacs/.emacs.custom.el
@@ -3,14 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
-     default))
- '(package-selected-packages nil)
- '(warning-minimum-level :error))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:height 1.7 :weight bold :underline t))))
+ '(org-level-1 ((t (:height 1.4 :weight bold :foreground "#6699cc"))))
+ '(org-level-2 ((t (:height 1.2 :weight bold :foreground "#99cc99"))))
+ '(org-level-3 ((t (:height 1.1 :weight bold :foreground "#f2777a")))))
